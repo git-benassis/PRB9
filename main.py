755eeef30d753350c30222cc6cbae5dded26e898
Victor Helppiquet <victor.helpiquet@domaine>
2026-03-19 11:55:27 +0100

    main

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ s0=1
 r=0.02
 sigma=0.2
 K=1 
-# plot.plot_brownian(W)
 
-S=gen.S(T,s0,r,sigma)
-plot.plot_brownian(S)
+# S=gen.S(T,s0,r,sigma)
+# plot.plot_brownian(S)
 
+S_values=gen.multi_S(T,s0,r,sigma,n)
+plot.plot_multi_S(S_values)
 # print(est.estimate_P1(n,T,1,0.02,0.2))
 
 # print(gen.multi_S(T,1,0.02,0.2,n))
