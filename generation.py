dfba951312cd0f04ecc129bb80e8044d42d3c6c5
Victor Helppiquet <victor.helpiquet@domaine>
2026-03-19 10:55:44 +0100

    brownian

--- generation.py
+++ generation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.random as npr
+import matplotlib.pyplot as plt
 
 def generate_uniform(n, dim = 1):
     return npr.uniform(size=(n, dim))
@@ -10,4 +11,12 @@ def generate_gaussian(n, dim = 1):
     return np.sqrt(-2 * np.log(U1)) * np.cos(2 * np.pi * U2)
 
 def generate_brownian_motion(n, dim = 1):
-    return np.cumsum(generate_gaussian(n, dim), axis=0)
+    Increments=generate_gaussian(n)
+    return np.concatenate((np.zeros(1),np.cumsum(Increments)))
+
+T=1
+n=100
+W=generate_brownian_motion(n)
+temps=np.linspace(0,T,n+1)
+plt.plot(temps,W)
+plt.show()
