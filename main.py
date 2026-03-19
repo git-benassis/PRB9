59fe7e0ad55f22371d54cc738b8c18e3b108296c
Victor Helppiquet <victor.helpiquet@domaine>
2026-03-19 11:38:45 +0100

    test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import plot, estimation as est
+import plot, estimation as est, generation as gen
 
 n=10 
 T=252*5 
 # plot.plot_brownian(n,T)
 
-print(est.estimate_P1(n,T,1,0.02,0.2,1))
+# print(est.estimate_P1(n,T,1,0.02,0.2))
+
+# print(gen.multi_S(T,1,0.02,0.2,n))
