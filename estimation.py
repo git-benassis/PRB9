15a60a9ce5a9403948748450ca384ec545278ade
nicola-henri.benassis@ensta.fr <benassis@BOOK-KRN2KBKV1T.localdomain>
2026-03-19 11:11:27 +0100

    Monte-Carlo prediction of P1

--- estimation.py
+++ estimation.py
@@ -1,9 +1,8 @@
 import generation as gen
 import numpy as np
 
-def estimate_P1(num_trials, r,T):
+def estimate_P1(num_trials, t,so,r,sigma):
     esp = 0
     for i in range(num_trials):
-        esp+=max(gen.S(),0)
-    return np.exp(-r*T)*esp/num_trials
-
+        esp+=max(gen.S(t,so,r,sigma),0)
+    return np.exp(-r*t)*esp/num_trials
