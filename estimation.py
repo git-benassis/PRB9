0b7c4fa5570c77d29635c0f5cc3700371dd9d496
Emmanuel-63 <emmanuel.gaillard@ensta.fr>
2026-03-19 11:28:15 +0100

    P1 exact

--- estimation.py
+++ estimation.py
@@ -7,7 +7,7 @@ def estimate_P1(num_trials, t,so,r,sigma):
         esp+=max(gen.S(t,so,r,sigma),0)
     return np.exp(-r*t)*esp/num_trials
 
-def calculate_P1(r,sigma,K,s0):
+def calculate_P1(r,sigma,K,s0,T):
     d1 = (np.log(s0/K)+(r+0.5*sigma**2)*T)/(sigma*np.sqrt(T))
     d2 = d1 - sigma*np.sqrt(T)
-    return K*np.exp(-r*T)*gen.repartition_gaussienne(-d2)-s0*repartition_gaussienne(-d1)
+    return K*np.exp(-r*T)*gen.repartition_gaussienne(-d2)-s0*gen.repartition_gaussienne(-d1)
